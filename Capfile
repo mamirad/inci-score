af432a1517b02de399ee06574d6c08a7e864a36e
mamirad <amirad2020@gmail.com>
2021-09-24 13:57:53 -0400

    make some changes

--- Capfile
+++ Capfile
@@ -48,4 +48,4 @@ require "capistrano/passenger"
 # Load custom tasks from `lib/capistrano/tasks` if you have any defined
 Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
 set :rbenv_type, :user
-set :rbenv_ruby, '2.6.1'
+set :rbenv_ruby, '2.6.8'
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '31.220.20.216/', user: 'deploy', roles: %w{app db web}
+server '31.220.20.216', user: 'deploy', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
